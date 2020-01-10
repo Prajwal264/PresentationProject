bf03bf39c4b92112bd50864b97351c574d38f420
Prajwal <prajwalp264@gmail.com>
2020-01-10 21:30:22 +0530

    fixed image bug

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get("/", function(req, res) {
 });
 
 // listener
-app.listen(process.env.PORT, process.env.IP, function() {
+app.listen(process.env.PORT || 3000, process.env.IP, function() {
   console.log("Server has started");
 });
