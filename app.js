f1a872740c296dca83b5e298abf9c4e6b6f2e945
Prajwal <prajwalp264@gmail.com>
2020-01-10 21:18:51 +0530

    added port listener

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get("/", function(req, res) {
 });
 
 // listener
-app.listen(3000, process.env.IP, function() {
+app.listen(3000 || process.env.PORT, process.env.IP, function() {
   console.log("Server has started");
 });
